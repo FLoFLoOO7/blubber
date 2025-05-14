2be8d31f37a19d26ecb5610dca25ad0559e9b17b
FLoFLoOO7 <florian.beldi@gmail.com>
2025-05-14 22:25:11 +0200

    blubber

--- sserver.py
+++ sserver.py
@@ -15,19 +15,12 @@ response = (
     
 )
 print(response)
-franzset=[
-    {"set_id":1,"vorderseite":"magasin","ruckseite":"laden","erledigt":False},
-    {"set_id":1,"vorderseite":"magasin","ruckseite":"laden","erledigt":False},
-    {"set_id":1,"vorderseite":"magasin","ruckseite":"laden","erledigt":False},
-    {"set_id":1,"vorderseite":"magasin","ruckseite":"laden","erledigt":False},
-    
 
-]
 
 app = Flask("Mein erster Server")
 CORS(app)
-
-@app.route("/api/add",methods=["PUT"])
+#Florian
+@app.route("/api/add_card",methods=["POST"])
 def add_item():
     new_card=request.get_json()
     response_add = (
@@ -35,40 +28,53 @@ def add_item():
         .insert(new_card)
         .execute()
     )
+    
     response = (
     supabase.table("Karteikarten")
     .select("*")
     .execute()
     
-)
-    print(response)
-print(response)
-#@app.route('/api/add', methods=['POST'])
-#def create_item():
- #   global id_count # Die Variable id_count in der Methode definiert
-  #  new_item = request.get_json() # Die Daten des neuen Items werden aus der Anfrage geladen
-   # new_item['id'] = id_count # Das neue Item bekommt eine ID
-    #id_count += 1 # Der ID-Zähler geht eins hoch, damit dass nächste Item eine neue ID bekommt
-#    items.append(new_item) # Das neue Item wird der Liste der Items angehängt
- #   response = jsonify({"message": "Item created", "items": items})
-  #  response.headers.add('Access-Control-Allow-Origin', '*')
-   # return response, 201
+    )
+    response=jsonify({"message": "Item created"})
+    response.headers.add("Access-Control-Allow-Origin","*")
+    return response, 201
+@app.route("/api/add_set",methods=["POST"])
+def add_set():
+    new_card=request.get_json()
+    response_add = (
+        supabase.table("Sets")
+        .insert(new_card)
+        .execute()
+    )
+    
+    response = (
+    supabase.table("Karteikarten")
+    .select("*")
+    .execute()
+    
+    )
+    response=jsonify({"message": "Item created"})
+    response.headers.add("Access-Control-Allow-Origin","*")
+    return response, 201
 
-#@app.route('/api/items/<int:item_id>', methods=['PUT'])
-#def update_item(item_id):
-#    print(item_id)
-    # Wir überprüfen, ob die item_id gültig ist. Das wird hier sehr einfach gemacht und müsste in echt noch etwas ausführlicher geprüft werden.
-#    if item_id >= len(items):
-    # Falls nicht geben wir eine Fehlermeldung zurück
-#        return jsonify({"error": "Item not found"}), 404
-    # Wir holen das geupdatete Item aus dem JSON der Anfrage
- #   updated_item = request.get_json()
-    # Und packen es in unserer Liste und überschreiben somit das bisherige item an dieser Stelle
-  #  items[item_id] = updated_item
-    # Zum schluss senden wir eine Erfolgsmeldung zurück sowie die aktualisierte Liste mit Items
-   # response = jsonify({"message": "Item updated", "items": items})
-#    response.headers.add('Access-Control-Allow-Origin', '*')
- #   return response
+@app.route("/api/cards/<int:set_id>", methods=["GET"])
+def loadSet(set_id):
+    response = (
+        supabase.table("Karteikarten")
+        .select("*")
+        .eq("SET_ID", set_id)
+        .execute()
+    )
+    cards = response.data
+    return jsonify(cards)
+
+
+
+@app.route("/api/sets", methods=["GET"])
+def get_sets():
+    response = supabase.table("Sets").select("*").execute()
+    sets = response.data
+    return jsonify(sets)
 
 if __name__ == '__main__':
     app.run(debug=True)
